4e4beb4cddc045e825a7719885cc47f4bf587f66
Sergei Grebnov <sergei.grebnov@gmail.com>
2025-01-18 19:06:12 -0800

    Fix stdc++ linking issue when using msvc tools

--- build.rs
+++ build.rs
@@ -80,6 +80,9 @@ fn main() -> Result<()> {
     } else {
         true
     };
+
+    let target = env::var("TARGET").expect("The 'TARGET' environment variable MUST be set");
+
     if should_compile {
         cu_files
             .par_iter()
@@ -99,7 +102,7 @@ fn main() -> Result<()> {
                     .arg("--compiler-options");
                 
                 // msvc tools require /bigobj
-                if env::var("TARGET") == Ok("x86_64-pc-windows-msvc".to_string()) {
+                if target.contains("msvc") {
                     command.arg("-fPIC,/bigobj");
                 } else {
                     command.arg("-fPIC");
@@ -154,7 +157,9 @@ fn main() -> Result<()> {
     println!("cargo:rustc-link-search={}", build_dir.display());
     println!("cargo:rustc-link-lib=layernorm");
     println!("cargo:rustc-link-lib=dylib=cudart");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+    if !target.contains("msvc") {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    }
 
     Ok(())
 }
